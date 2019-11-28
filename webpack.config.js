f380d8a523d8192474718374847d93e9e881b3a9
Viktor Fonic <viktor.fonic@gmail.com>
2019-11-28 10:03:57 +0100

    Upgrade to babel 7

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
         use: [{
           loader: 'babel-loader',
           options: {
-            "presets": [["es2015", {"modules": false}]]
+            "presets": ['@babel/preset-env']
           }
         }]
       },
