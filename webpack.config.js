078926e9555bc99d67c53450b4a39c375d8bbc8d
JB <jb@visualjerk.de>
2020-09-12 20:39:04 +0200

    chore: bump serialize-javascript

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,41 @@
 module.exports = {
-  entry: '/src/index.js',
+  entry: './src/index.js',
   output: {
     path: __dirname,
     libraryTarget: 'umd',
-    filename: './dist/index.js'
+    filename: './dist/index.js',
   },
   devServer: {
-    contentBase: './demo'
+    contentBase: './demo',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  targets: '> 0.25%, not dead',
-                }
-              ]
-            ]
-          }
-        }]
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: '> 0.25%, not dead',
+                  },
+                ],
+              ],
+            },
+          },
+        ],
       },
       {
         test: /\.svg$/,
-        use: [{
-          loader: 'raw-loader'
-        }]
-      }
-    ]
-  }
-};
+        use: [
+          {
+            loader: 'raw-loader',
+          },
+        ],
+      },
+    ],
+  },
+}
