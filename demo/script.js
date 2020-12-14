28a286ee50597281d45c86ba650a15e96df2b22c
Heiko Rutz <heiko.rutz@xi2.de>
2020-12-14 13:55:48 +0100

    Harmonize whitespaces

--- demo/script.js
+++ demo/script.js
@@ -1,10 +1,10 @@
 var quill = new Quill('#editor', {
-	theme: 'snow',
-	modules: {
-		magicUrl: {
+  theme: 'snow',
+  modules: {
+    magicUrl: {
       globalRegularExpression: /(https?:\/\/|www\.|mailto:|tel:)[\S]+/g,
       urlRegularExpression: /(https?:\/\/[\S]+)|(www.[\S]+)|(mailto:[\S]+)|(tel:[\S]+)/
     }
-	},
-	placeholder: 'Paste or type a url ...'
+  },
+  placeholder: 'Paste or type a url ...'
 });
