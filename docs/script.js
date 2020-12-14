3c5589ead562751cfd47f3b0361d8333293f2306
Heiko Rutz <heiko.rutz@xi2.de>
2020-12-14 14:05:09 +0100

    Update readme, docs and demo

--- docs/script.js
+++ docs/script.js
@@ -2,8 +2,8 @@ var quill = new Quill('#editor', {
   theme: 'snow',
   modules: {
     magicUrl: {
-      globalRegularExpression: /(https?:\/\/|www\.|mailto:|tel:)[\S]+/g,
-      urlRegularExpression: /(https?:\/\/[\S]+)|(www.[\S]+)|(mailto:[\S]+)|(tel:[\S]+)/
+      globalRegularExpression: /(https?:\/\/|www\.|mailto:|tel:)[\S]+/gi,
+      urlRegularExpression: /(https?:\/\/[\S]+)|(www.[\S]+)|(mailto:[\S]+)|(tel:[\S]+)/i
     }
   },
   placeholder: 'Paste or type a url ...'
