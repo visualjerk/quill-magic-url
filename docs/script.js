aa01566828bafadfb5735dc3e6c1b2ca231d8453
Heiko Rutz <heiko.rutz@xi2.de>
2021-01-18 15:44:11 +0100

    Use defaults in demo and docs

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,7 @@
 var quill = new Quill('#editor', {
   theme: 'snow',
   modules: {
-    magicUrl: {
-      globalRegularExpression: /(https?:\/\/|www\.|mailto:|tel:)[\S]+/gi,
-      urlRegularExpression: /(https?:\/\/[\S]+)|(www.[\S]+)|(mailto:[\S]+)|(tel:[\S]+)/i
-    }
+    magicUrl: true
   },
   placeholder: 'Paste or type a url ...'
 });
